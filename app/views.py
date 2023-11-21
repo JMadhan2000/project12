404fe9f29335099f1940dd4651950e8e0ac099d9
JMadhan2000 <jmadhanmohanreddy8500@gmail.com>
2023-11-21 08:52:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def aggipette_macha(request):
+    return render(request,'aggipette_macha.html')
+
+
+def rakeshmaster(request):
+    return render(request,'rakeshmaster.html')
+
+
+
+def avesham_star(request):
+    return render(request,'avesham_star.html')
